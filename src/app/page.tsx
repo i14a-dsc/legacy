8a79d348566cbd9ea0da7ee35855d483e376b195
I14A <dev.i14a.dsc@gmail.com>
2026-06-27 14:28:09 +0900

    chore: fix preload issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ export default function Home() {
   return (
     <div>
       <p>バトラ「うんち！ｗ」</p>
-      <video src="/output3.mp4" width="499.2" height="288" controls></video>
+      <video src="/output3.mp4" width="499.2" height="288" preload="none" controls>
+        ご使用のブラウザはビデオタグをサポートしていません。
+      </video>
     </div>
   );
 }
